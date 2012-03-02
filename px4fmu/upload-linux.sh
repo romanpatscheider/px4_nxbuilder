71a2dc646e4d08cfd86515c5598e07e24c4f27d7
pixhawk <pixhawk@pixhawk07.(none)>
2012-03-02 19:40:45 +0100

    added upload-linux make target

---
+++ px4fmu/upload-linux.sh
@@ -0,0 +1,4 @@
+cd ~/pixhawk/px_nxbuilder/px4fmu
+~/pixhawk/px4_bootloader/px_mkfw.py --board_id 5 > prototype.px4
+~/pixhawk/px4_bootloader/px_mkfw.py --prototype prototype.px4 --image ~/pixhawk/px_nxbuilder/build/px4fmu/nuttx/nuttx.bin > nuttx.px4
+~/pixhawk/px4_bootloader/px_uploader.py nuttx.px4 --port /dev/ttyACM0

6ed13cff8409f124b36de97f5f76f89fe92c5de6
Thomas Gubler <thomasgubler@student.ethz.ch>
2012-03-08 17:21:59 +0100

    started porting code from gps_test

--- px4fmu/apps/gps/gps.c
+++ px4fmu/apps/gps/gps.c
@@ -39,9 +39,12 @@
 
 
 #include <nuttx/config.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include "gps.h"
 #include "nmealib/nmea/nmea.h"
+
 
 
 
@@ -58,35 +61,49 @@
 int gps_main(int argc, char *argv[])
 {
     // print text
-    printf("Hello, gps2!!\n");
+    printf("Hello, gps!!\n");
     usleep(100000);
 
-    //example nmea parse
-    const char *buff[] = {
-        "$GPRMC,173843,A,3349.896,N,11808.521,W,000.0,360.0,230108,013.4,E*69\r\n",
-        "$GPGGA,111609.14,5001.27,N,3613.06,E,3,08,0.0,10.2,M,0.0,M,0.0,0000*70\r\n",
-        "$GPGSV,2,1,08,01,05,005,80,02,05,050,80,03,05,095,80,04,05,140,80*7f\r\n",
-        "$GPGSV,2,2,08,05,05,185,80,06,05,230,80,07,05,275,80,08,05,320,80*71\r\n",
-        "$GPGSA,A,3,01,02,03,04,05,06,07,08,00,00,00,00,0.0,0.0,0.0*3a\r\n",
-        "$GPRMC,111609.14,A,5001.27,N,3613.06,E,11.2,0.0,261206,0.0,E*50\r\n",
-        "$GPVTG,217.5,T,208.8,M,000.00,N,000.01,K*4C\r\n"
-    };
+    //read arguments
+    char * device = argv[1];
+    int baud = atoi(argv[2]);
 
-    int it;
-    nmeaINFO info;
-    nmeaPARSER parser;
+    int buffer_size = 500;
+    nmeaINFO * info = malloc(sizeof(nmeaINFO));
 
-    nmea_zero_INFO(&info);
-    nmea_parser_init(&parser);
+    //open port (baud rate is set in defconfig file)
+	int fd = open_port(device);
+	printf("port opened: %s\n", device);
 
-    printf("starting parsing...\n");
-    sleep(2);
-    for(it = 0; it < 6; ++it)
-    {
-        nmea_parse(&parser, buff[it], (int)strlen(buff[it]), &info);
-        printf("Sig:%d\n",info.sig);
-    }
-    nmea_parser_destroy(&parser);
+	//gps parser (nmea)
+	nmeaPARSER parser;
+	nmea_parser_init(&parser);
+	nmea_zero_INFO(info);
+	double lat_dec = 0;
+	double lon_dec = 0;
+
+	while(1)
+	{
+		char * gps_rx_buffer = malloc(buffer_size*sizeof(char));
+
+		//get gps data into info
+//		read_gps_nmea(fd, gps_rx_buffer, buffer_size, info, &parser);
+
+		//convert latitude longitude
+//		lat_dec = nmea_ndeg2degree(info->lat);
+//		lon_dec = nmea_ndeg2degree(info->lon);
+
+		//Test output
+		printf("Lat:%f, Lon:%f,Elev:%f, Sig:%d, Fix:%d, Inview:%d\n", lat_dec, lon_dec, info->elv, info->sig, info->fix, info->satinfo.inview);
+		free( gps_rx_buffer );
+
+	}
+
+
+
+	//close port
+	close_port(fd);
+
 
     return 0;
 }

1791121e72d6762a7f865751863ac95d0519f56c
Thomas Gubler <thomasgubler@student.ethz.ch>
2012-03-20 15:14:59 +0100

    deleted old apps/mq_config.h

--- px4fmu/apps/mq_config.h
+++
@@ -1,9 +0,0 @@
-//Configuration for mq beween different apps
-
-#ifndef MQ_CONFIG_H_
-#define MQ_CONFIG_H_
-
-#define MQ_ATTR_GPS { .mq_flags = 0, .mq_maxmsg = 10, .mq_msgsize = 2, .mq_curmsgs = 0 }
-#define MQ_NAME_GPS "gps_queue"
-
-#endif /* MQ_CONFIG_H_ */
